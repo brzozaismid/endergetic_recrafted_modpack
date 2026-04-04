aee03cc8bed3749180d3cb8f14cecc8319777e1e
brzoza <b.brzozowski199@gmail.com>
2026-04-04 11:15:30 +0200

    Update HeightmapSpreadDoublePlacement.java

--- src/main/java/com/teamabnormals/endergetic/common/levelgen/placement/HeightmapSpreadDoublePlacement.java
+++ src/main/java/com/teamabnormals/endergetic/common/levelgen/placement/HeightmapSpreadDoublePlacement.java
@@ -30,8 +30,8 @@ public final class HeightmapSpreadDoublePlacement extends PlacementModifier {
 	public Stream<BlockPos> getPositions(PlacementContext context, RandomSource random, BlockPos pos) {
 		int x = pos.getX();
 		int z = pos.getZ();
-		int height = context.getHeight(this.heightmap, x, z);
-		return height == 0 ? Stream.of() : Stream.of(new BlockPos(x, random.nextInt(height * 2), z));
+        int min = context.getMinBuildHeight();
+		return height == 0 ? Stream.of() : Stream.of(new BlockPos(x, random.nextInt((height * 2) - min) + min, z));
 	}
 
 	@Override
